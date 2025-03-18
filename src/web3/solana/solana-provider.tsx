ad3b135b1396747327c79e37c434739df3dfce8d
nitishxyz <nitishxyz@proton.me>
2025-03-18 23:13:57 +0530

    hardware wallet adapters

--- src/web3/solana/solana-provider.tsx
+++ src/web3/solana/solana-provider.tsx
@@ -11,6 +11,13 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  UnsafeBurnerWalletAdapter,
+  LedgerWalletAdapter,
+  SafePalWalletAdapter,
+  TrezorWalletAdapter,
+  KeystoneWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
 import { ReactNode, useCallback, useMemo } from "react";
 import { useCluster } from "../../web3/cluster/cluster-data-access";
 
@@ -31,9 +38,21 @@ export function SolanaProvider({ children }: { children: ReactNode }) {
     console.error(error);
   }, []);
 
+  const wallets = useMemo(
+    () => [
+      new UnsafeBurnerWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new SafePalWalletAdapter(),
+      new TrezorWalletAdapter(),
+      new KeystoneWalletAdapter(),
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]} onError={onError} autoConnect={true}>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect={true}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
